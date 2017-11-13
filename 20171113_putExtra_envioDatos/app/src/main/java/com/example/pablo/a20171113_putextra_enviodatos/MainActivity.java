937d686c890489664bf314efaab4b975a61dd732
SgtNaboloco <pablodiazsjm@hotmail.com>
2017-11-13 17:25:24 +0100

    putExtra

---
+++ 20171113_putExtra_envioDatos/app/src/main/java/com/example/pablo/a20171113_putextra_enviodatos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pablo.a20171113_putextra_enviodatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
